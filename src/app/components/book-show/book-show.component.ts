28c845521614af8087dfc057be7174c418277b04
Rejda <qati.rejda@gmail.com>
2024-12-05 10:30:30 +0100

    first push

---
+++ src/app/components/book-show/book-show.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-book-show',
+  imports: [],
+  templateUrl: './book-show.component.html',
+  styleUrl: './book-show.component.css'
+})
+export class BookShowComponent {
+
+}
