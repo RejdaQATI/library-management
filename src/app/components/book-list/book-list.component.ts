dc8397900abdb6784157f050e63e470774e4e814
RED <qati.rejda@gmail.com>
2024-12-06 10:48:41 +0100

    feat: filters done

--- src/app/components/book-list/book-list.component.ts
+++ src/app/components/book-list/book-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms'; 
 import { BookService } from '../../services/book.service';
 import { Book } from '../../models/book.model';
 import { HeaderComponent } from '../header/header.component';
@@ -8,24 +9,43 @@ import { HeaderComponent } from '../header/header.component';
 @Component({
   selector: 'app-book-list',
   standalone: true,
-  imports: [CommonModule, RouterModule, HeaderComponent],
+  imports: [CommonModule, RouterModule, HeaderComponent, FormsModule],
   templateUrl: './book-list.component.html',
   styleUrls: ['./book-list.component.css'],
 })
 export class BookListComponent implements OnInit {
   books: Book[] = [];
+  filteredBooks: Book[] = [];
+
+  authorFilter: string = '';
+  genreFilter: string = '';
 
   constructor(private bookService: BookService) {}
 
   ngOnInit(): void {
     this.bookService.getBooks().subscribe((data) => {
       this.books = data;
+      this.filteredBooks = data; 
     });
   }
 
   deleteBook(id: string): void {
     this.bookService.deleteBook(id).subscribe(() => {
       this.books = this.books.filter((book) => book.id !== id);
+      this.filterBooks(); 
+    });
+  }
+
+  filterBooks(): void {
+    this.filteredBooks = this.books.filter((book) => {
+      const matchesAuthor = this.authorFilter
+        ? book.author.toLowerCase().includes(this.authorFilter.toLowerCase())
+        : true;
+      const matchesGenre = this.genreFilter
+        ? book.genre.toLowerCase().includes(this.genreFilter.toLowerCase())
+        : true;
+
+      return matchesAuthor && matchesGenre;
     });
   }
 }
